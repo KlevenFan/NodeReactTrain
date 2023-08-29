470e8cd79723b8cc62ea52610da744499c452b42
fansuyu <fansuyu@meituan.com>
2023-08-30 01:29:44 +0800

    feat: 第6节 egg-jwt 实现用户鉴权（注册、登录）-- 完成

--- app/controller/user.js
+++ app/controller/user.js
@@ -38,6 +38,60 @@ class UserController extends Controller {
       }
     }
   }
+
+  async login() {
+    const {ctx, app} = this;
+    const {username, password} = ctx.request.body;
+
+    const userInfo = await ctx.service.user.getUserByName(username);
+
+    if(!userInfo || !userInfo.id) {
+      ctx.body = {
+        code: 500,
+        msg: '账号不存在',
+        data: null
+      }
+      return;
+    }
+
+    if(userInfo && password !== userInfo.password) {
+      ctx.body = {
+        code: 500,
+        msg: '账号密码错误',
+        data: null
+      }
+      return
+    }
+
+    const token = app.jwt.sign({
+      id: userInfo.id,
+      username: userInfo.username,
+      exp: Math.floor((Date.now()/1000) + (24 * 60 * 60)),
+    }, app.config.jwt.secret)
+
+    ctx.body = {
+      code:200,
+      message: '登陆成功',
+      data: {
+        token
+      }
+    }
+  }
+
+  async test() {
+    const {ctx, app} = this;
+    const token = ctx.request.header.authorization;
+
+    const decode = await app.jwt.verify(token, app.config.jwt.secret);
+
+    ctx.body = {
+      code: 200,
+      message: '获取成功',
+      data: {
+        ...decode
+      }
+    }
+  }
 }
 
 module.exports = UserController;
--- app/router.js
+++ app/router.js
@@ -4,7 +4,7 @@
  * @param {Egg.Application} app - egg application
  */
 module.exports = app => {
-  const { router, controller } = app;
+  const { router, controller, middleware } = app;
   // router.get('/', controller.home.index);
   // router.get('/user/:id', controller.home.user);
   // router.post('/add', controller.home.add);
@@ -12,6 +12,9 @@ module.exports = app => {
   // router.post('/add_user', controller.home.addUser);
   // router.post('/edit_user', controller.home.editUser);
   // router.post('/delete_user', controller.home.deleteUser);
+  const _jwt = middleware.jwtErr(app.config.jwt.secret);
 
   router.post('/api/user/register', controller.user.register);
+  router.post('/api/user/login', controller.user.login);
+  router.get('/api/user/test', _jwt, controller.user.test);
 };
